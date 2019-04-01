ba5cf1c30266e5b8ca3de075e0eb6b12071af7c1
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-01 11:50:49 +0300

    get API key and fetch items

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from '../../config';
 import MovieImage from '../MovieImage/MovieImage';
 import FourColGrid from '../FourColGrid/FourColGrid';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
@@ -9,6 +16,53 @@ import Spinner from '../Spinner/Spinner';
 import './Home.css';
 
 class Home extends Component {
+  state = {
+    movies: [],
+    movieImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  loadMoreItems = () => {
+    const { searchTerm, currentPage } = this.state;
+
+    let endpoint = '';
+    this.setState({ loading: true });
+
+    if (searchTerm === '') {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage +
+        1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage +
+        1}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
+  fetchItems = endpoint => {
+    const { movies, movieImage } = this.state;
+
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          movies: [...movies, ...result.results],
+          movieImage: movieImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
+      });
+  };
+
   render() {
     return (
       <div className="movie-home">
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'f98dc1d273be0526711770a13fba0bdc';
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+const BACKDROP_SIZE = 'w1280';
+
+const POSTER_SIZE = 'w500';
+
+export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
